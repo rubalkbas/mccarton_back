d3ed6e8725ed095199c6a4fddd4427798d496e55
Pedro <pedro@indra>
2023-03-23 12:19:45 -0600

    cambios

--- src/main/java/com/mccarton/controller/RegistroController.java
+++ src/main/java/com/mccarton/controller/RegistroController.java
@@ -1,10 +1,13 @@
 package com.mccarton.controller;
 
+import java.util.List;
+
 import javax.mail.MessagingException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,7 +18,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.mccarton.model.dto.SingleResponse;
 import com.mccarton.model.entity.ClienteEntity;
+import com.mccarton.model.entity.DireccionEntity;
 import com.mccarton.model.entity.UsuarioEntity;
 import com.mccarton.service.RegistroService;
 import com.mccarton.service.UsuarioService;
@@ -28,14 +33,16 @@ public class RegistroController {
 	@Autowired
     private RegistroService registroService;
 
-    @PostMapping(path = "/usuario", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Void> registerUser(@ModelAttribute ClienteEntity user) throws MessagingException {
-    	registroService.registerUser(user);
-        return ResponseEntity.ok().build();
+    @PostMapping(path = "/cliente", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<SingleResponse<ClienteEntity>> registerUser(@ModelAttribute ClienteEntity user) throws MessagingException {
+    	
+    	SingleResponse<ClienteEntity> response = new SingleResponse<>();
+    	response = registroService.registerUser(user);
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     @GetMapping(path = "/confirmar/{idUsuario}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Void> confirmRegistration(@PathVariable("idUsuario") Integer idUsuario) {
+    public ResponseEntity<Void> confirmRegistration(@PathVariable("idUsuario") String idUsuario) {
     	registroService.confirmRegistration(idUsuario);
         return ResponseEntity.ok().build();
     }
