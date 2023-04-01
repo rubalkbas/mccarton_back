562250126fb83ea6dd8ecbe97a55992df29db889
JJ <JJ@fsw.com.mx>
2023-03-31 18:33:35 -0600

    Oferta CRUD

---
+++ src/main/java/com/mccarton/controller/OfertaController.java
@@ -0,0 +1,61 @@
+package com.mccarton.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mccarton.model.dto.SingleResponse;
+import com.mccarton.model.entity.OfertaEntity;
+import com.mccarton.service.IOfertaService;
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/oferta")
+public class OfertaController {
+
+	@Autowired
+	private IOfertaService ofertaService;
+	
+	@GetMapping(value = "/consultarTodos", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<SingleResponse<List<OfertaEntity>>> consultarTodos(){
+		SingleResponse<List<OfertaEntity>> response = ofertaService.consultarTodosOferta();
+		return new ResponseEntity<SingleResponse<List<OfertaEntity>>>(response, HttpStatus.OK);
+	}
+	
+	@PostMapping(value = "/guardar",produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<SingleResponse<OfertaEntity>> actualizarOfertaEstatus(@RequestBody OfertaEntity oferta, @RequestParam Integer idProducto){
+		SingleResponse<OfertaEntity> response = ofertaService.guardarOferta(oferta, idProducto);
+		return new ResponseEntity<SingleResponse<OfertaEntity>>(response, HttpStatus.OK);
+	}
+	
+	@PutMapping(value = "/actualizar",produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<SingleResponse<OfertaEntity>> actualizarOferta(@RequestBody OfertaEntity oferta, @RequestParam Integer idProducto){
+		SingleResponse<OfertaEntity> response = ofertaService.actualizarOferta(oferta, idProducto);
+		return new ResponseEntity<SingleResponse<OfertaEntity>>(response, HttpStatus.OK);
+	}
+	
+	@PutMapping(value = "/actualizarActivo",produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<SingleResponse<OfertaEntity>> actualizarOfertaEstatus(@RequestParam Integer idOferta,
+			@RequestParam Integer estatus){
+		SingleResponse<OfertaEntity> response = ofertaService.actualizarOfertaEstatus(idOferta, estatus);
+		return new ResponseEntity<SingleResponse<OfertaEntity>>(response, HttpStatus.OK);
+	}
+	
+	
+	@GetMapping(value = "/consultarTodosActivos", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<SingleResponse<List<OfertaEntity>>> consultarTodosActivos(){
+		SingleResponse<List<OfertaEntity>> response = ofertaService.consultarTodosActivos();
+		return new ResponseEntity<SingleResponse<List<OfertaEntity>>>(response, HttpStatus.OK);
+	}
+}
---
+++ src/main/java/com/mccarton/model/entity/OfertaEntity.java
@@ -0,0 +1,60 @@
+package com.mccarton.model.entity;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "OFERTAS")
+public class OfertaEntity {
+
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "ID_OFERTA")
+	private Integer idOferta;
+	
+	@Column(name = "TIPO_OFERTA", length = 50)
+	private String tipoOferta;
+	
+	@Column(name = "DESCUENTO_PORCENTAJE", length = 10)
+	private Integer descuentoEnPorcentaje;
+	
+	@Column(name = "FECHA_INICIO")
+	private LocalDateTime fechaInicio;
+	
+	@Column(name = "FECHA_FIN")
+	private LocalDateTime fechaFin;
+	
+	@Column(name = "CODIGO_OFERTA",length = 10)
+	private String codigoOferta;
+	
+	@Column(name = "DESCRIPCION",length = 100)
+	private String descripcion;
+	
+	@Column(name = "CONDICION_OFERTA", length = 100)
+	private String condicionesOferta;
+	
+	@Column(name = "NUMERO_USO")
+	private Integer numeroUso;
+	
+	@Column(name = "ESTATUS",length = 1)
+	private Integer estatus;
+	
+	@OneToOne(optional = false)
+	@JoinColumn(name = "ID_PRODUCTO")
+	private ProductosEntity producto;
+		
+}
